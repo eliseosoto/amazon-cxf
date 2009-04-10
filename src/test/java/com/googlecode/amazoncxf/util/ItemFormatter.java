6265e9b6d79a4e11461cfc39617ee07bb065d4d6
Eliseo Soto <eliseosoto@users.noreply.github.com>
2009-04-10 18:21:28 +0000

    Basic DAO with some unit tests.

---
+++ src/test/java/com/googlecode/amazoncxf/util/ItemFormatter.java
@@ -0,0 +1,24 @@
+package com.googlecode.amazoncxf.util;
+
+import com.amazon.webservices.awsecommerceservice._2009_02_01.Item;
+
+public class ItemFormatter {
+	public static String formatItem(Item item) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("ASIN: " + item.getASIN());
+		sb.append(", Name: " + item.getItemAttributes().getTitle());
+		if (item.getOffers().getTotalOffers().intValue() > 0) {
+			sb.append(", Price: "
+					+ item.getOffers().getOffer().get(0).getOfferListing().get(
+							0).getPrice().getFormattedPrice());
+			sb.append(", Qty: "
+					+ item.getOffers().getOffer().get(0).getOfferListing().get(
+							0).getQuantity());
+		}
+		sb.append(", Manufacturer: "
+				+ item.getItemAttributes().getManufacturer());
+
+		return sb.toString();
+	}
+}
