e1fc8ff2803892139c9c6e18716cd7616c4d03c8
eliseosoto <eliseosoto@50adbb1e-1fa6-11de-ac9f-1d9250355bca>
2009-04-15 15:20:30 +0000

    Added the missing files.

---
+++ trunk/src/main/java/com/googlecode/amazoncxf/dao/ItemDao.java
@@ -0,0 +1,40 @@
+package com.googlecode.amazoncxf.dao;
+
+import java.util.List;
+
+import com.amazon.webservices.awsecommerceservice._2009_02_01.Item;
+
+/**
+ * Operations that can be performed on an Item, it uses the webservices to
+ * populate the data.
+ * 
+ * @author eliseo.soto
+ * 
+ */
+public interface ItemDao {
+	/**
+	 * Returns the Item data for the given ASIN
+	 * 
+	 * @param asin
+	 *            A valid ASIN
+	 * @return
+	 */
+	Item lookup(String asin);
+
+	/**
+	 * Gets the Item data for several ASINs at once
+	 * 
+	 * @param asins
+	 *            Collection of valid ASINs
+	 * @return
+	 */
+	List<Item> getItems(List<String> asins);
+
+	/**
+	 * Searches items by keyword in all categories.
+	 * 
+	 * @param keyword
+	 * @return
+	 */
+	List<Item> searchItems(String keyword);
+}
