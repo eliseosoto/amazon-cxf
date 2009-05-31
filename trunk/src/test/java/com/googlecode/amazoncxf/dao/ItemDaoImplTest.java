1f0e6b8df93f625951985180cab3e85bf090652b
eliseosoto <eliseosoto@50adbb1e-1fa6-11de-ac9f-1d9250355bca>
2009-05-31 16:32:32 +0000

    Added a new test that makes a method fail.

--- trunk/src/test/java/com/googlecode/amazoncxf/dao/ItemDaoImplTest.java
+++ trunk/src/test/java/com/googlecode/amazoncxf/dao/ItemDaoImplTest.java
@@ -19,87 +19,101 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.amazon.webservices.awsecommerceservice._2009_02_01.Item;
 import com.googlecode.amazoncxf.util.ItemFormatter;
+import javax.swing.plaf.ListUI;
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.util.CollectionUtils;
 
 public class ItemDaoImplTest {
-	private static Log log = LogFactory.getLog(ItemDaoImplTest.class);
-	static private ItemDao itemDao;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		ApplicationContext factory = new ClassPathXmlApplicationContext(
-				"applicationContext.xml");
-		itemDao = (ItemDao) factory.getBean("itemDao");
-		log.debug(((ItemDaoImpl) itemDao)
-				.getAmazonAssociatesWebServiceAccount());
-	}
+    private static Log log = LogFactory.getLog(ItemDaoImplTest.class);
+    static private ItemDao itemDao;
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        ApplicationContext factory = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+        itemDao = (ItemDao) factory.getBean("itemDao");
+        log.debug(((ItemDaoImpl) itemDao).getAmazonAssociatesWebServiceAccount());
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	public void testLookup() throws Exception {
-		Item item = itemDao.lookup("B000FQ9QVI");
-		log.debug(ItemFormatter.formatItem(item));
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testGetItems() throws Exception {
-		List<String> asins = new ArrayList<String>();
-		asins.add("B000FQ9QVI");
-		asins.add("B000VWYJ86");
-		asins.add("B000TFINY6");
-		asins.add("B000JLKIHA");
-		asins.add("B0009RGLSE");
-		asins.add("0060762012");
-		asins.add("B0011TQLA2");
-		asins.add("B000NNK4DM");
-		asins.add("B000P297ES");
-		asins.add("1932394885");
-		List<Item> items = itemDao.getItems(asins);
+    @Test
+    public void testLookup() throws Exception {
+        Item item = itemDao.lookup("B000FQ9QVI");
+        log.debug(ItemFormatter.formatItem(item));
+    }
 
-		for (Item item : items) {
-			log.debug(ItemFormatter.formatItem(item));
-		}
-		assertNotNull(items);
-		assertEquals(items.size(), asins.size());
-	}
+    @Test
+    public void testGetItems() throws Exception {
+        log.debug("testGetItems");
+        List<String> asins = new ArrayList<String>();
+        asins.add("B000FQ9QVI");
+        asins.add("B000VWYJ86");
+        asins.add("B000TFINY6");
+        asins.add("B000JLKIHA");
+        asins.add("B0009RGLSE");
+        asins.add("0060762012");
+        asins.add("B0011TQLA2");
+        asins.add("B000NNK4DM");
+        asins.add("B000P297ES");
+        asins.add("1932394885");
+        List<Item> items = itemDao.getItems(asins);
 
-	@Test
-	public void testSearchItems() throws Exception {
-		log.debug("testSearchItems");
-		List<Item> items = itemDao.searchItems("ps3");
+        for (Item item : items) {
+            log.debug(ItemFormatter.formatItem(item));
+        }
+        assertNotNull(items);
+        assertEquals(items.size(), asins.size());
 
-		assertNotNull(items);
-		assertTrue(items.size() > 0);
-		for (Item item : items) {
-			log.debug(ItemFormatter.formatItem(item));
-		}
-	}
+        log.debug("testGetItems-more");
+        List<String> moreAsins = CollectionUtils.arrayToList(new String[]{"B001992NUQ", "B001ABN82K", "B001DSNF8C", "B001E0RZ3U", "B001E2D44W", "B001IVXI7C", "B001LF2WCC", "B001OQCV74", "B001PKHRTG", "B001QIVEVE"});
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testGetItemsTooMany() throws Exception {
-		// Now try to do it with even more ASINS
-		List<String> asins = new ArrayList<String>();
-		asins.add("B000FQ9QVI");
-		asins.add("B000VWYJ86");
-		asins.add("B000TFINY6");
-		asins.add("B000JLKIHA");
-		asins.add("B0009RGLSE");
-		asins.add("0060762012");
-		asins.add("B0011TQLA2");
-		asins.add("B000NNK4DM");
-		asins.add("B000P297ES");
-		asins.add("1932394885");
-		asins.add("B000KICN7U");
-		itemDao.getItems(asins);
-	}
+        items = itemDao.getItems(moreAsins);
+        for (Item item : items) {
+            log.debug(ItemFormatter.formatItem(item));
+        }
+        assertNotNull(items);
+        assertEquals(items.size(), moreAsins.size());
+    }
+
+    @Test
+    public void testSearchItems() throws Exception {
+        log.debug("testSearchItems");
+        List<Item> items = itemDao.searchItems("ps3");
+
+        assertNotNull(items);
+        assertTrue(items.size() > 0);
+        for (Item item : items) {
+            log.debug(ItemFormatter.formatItem(item));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetItemsTooMany() throws Exception {
+        // Now try to do it with even more ASINS
+        List<String> asins = new ArrayList<String>();
+        asins.add("B000FQ9QVI");
+        asins.add("B000VWYJ86");
+        asins.add("B000TFINY6");
+        asins.add("B000JLKIHA");
+        asins.add("B0009RGLSE");
+        asins.add("0060762012");
+        asins.add("B0011TQLA2");
+        asins.add("B000NNK4DM");
+        asins.add("B000P297ES");
+        asins.add("1932394885");
+        asins.add("B000KICN7U");
+        itemDao.getItems(asins);
+    }
 }
