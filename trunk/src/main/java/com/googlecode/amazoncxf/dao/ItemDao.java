d3496995b3e94fe230401be57335643fcf63c0b3
eliseosoto <eliseosoto@50adbb1e-1fa6-11de-ac9f-1d9250355bca>
2009-06-03 21:39:17 +0000

    Upgraded to CXF 2.2.2 and added method overloads for the lookup and search operations.

--- trunk/src/main/java/com/googlecode/amazoncxf/dao/ItemDao.java
+++ trunk/src/main/java/com/googlecode/amazoncxf/dao/ItemDao.java
@@ -22,6 +22,16 @@ public interface ItemDao {
 	Item lookup(String asin);
 
 	/**
+	 * Returns the Item data for the given ASIN and the specified Response
+	 * Groups
+	 * 
+	 * @param asin
+	 * @param responseGroups
+	 * @return
+	 */
+	Item lookup(String asin, List<String> responseGroups);
+
+	/**
 	 * Gets the Item data for several ASINs at once
 	 * 
 	 * @param asins
@@ -31,10 +41,52 @@ public interface ItemDao {
 	List<Item> getItems(List<String> asins);
 
 	/**
+	 * Gets the Item data for several ASINs at once and the specified Response
+	 * Groups
+	 * 
+	 * @param asins
+	 * @param responseGroups
+	 * @return
+	 */
+	List<Item> getItems(List<String> asins, List<String> responseGroups);
+
+	/**
 	 * Searches items by keyword in all categories.
 	 * 
 	 * @param keyword
 	 * @return
 	 */
 	List<Item> searchItems(String keyword);
+
+	/**
+	 * Searches items by keyword in all categories and the specified Response
+	 * Groups
+	 * 
+	 * @param keyword
+	 * @param responseGroups
+	 * @return
+	 */
+	List<Item> searchItems(String keyword, List<String> responseGroups);
+
+	/**
+	 * Searches items by keyword in all categories and the specified Search
+	 * Index
+	 * 
+	 * @param keyword
+	 * @param searchIndex
+	 * @return
+	 */
+	List<Item> searchItems(String keyword, String searchIndex);
+
+	/**
+	 * Searches items by keyword in all categories and the specified Search
+	 * Index and Response Group
+	 * 
+	 * @param keyword
+	 * @param responseGroups
+	 * @param searchIndex
+	 * @return
+	 */
+	List<Item> searchItems(String keyword, List<String> responseGroups,
+			String searchIndex);
 }
